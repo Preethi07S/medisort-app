9a7ff293ab8c27431a933ef43c67bacc47672806
Preethi07S <preethisree53@gmail.com>
2026-05-25 23:42:53 +0530

    this will work

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,5 @@ import MediSort_AI from './MediSort_AI';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <MediSort_AI />
-  </React.StrictMode>
 );
